e4384301eec1250fe7d06af158aafc3db7678006
khertopert <khert0pert@yahoo.com>
2023-07-23 10:22:27 +0200

    delete unused files

--- react-app/src/state/actions/counter-actions.ts
+++
@@ -1,4 +0,0 @@
-type CounterAction = {
-    type: 'increase' | 'decrease';
-    value: number;
-}
--- react-app/src/state/reducers/index.ts
+++
@@ -1,4 +0,0 @@
-import { combineReducers } from "redux";
-import { counterReducer } from "./counter-reducer";
-
-export const reducers = combineReducers(counterReducer);
